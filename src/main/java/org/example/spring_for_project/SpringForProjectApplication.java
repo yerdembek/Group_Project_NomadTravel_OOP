30a5724b06e19f7ec70ebe8a877eca4d99430820
mrRealist <erdembekb@gmail.com>
2025-01-24 00:18:14 +0500

    I added a database and a table to store tours and of course created a database and made a dependency

---
+++ src/main/java/org/example/spring_for_project/SpringForProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_for_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForProjectApplication.class, args);
+    }
+
+}
